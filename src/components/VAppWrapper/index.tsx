c132d400c5935c3fdc23102580201edab8c96610
Johnny Noble <github-a3bd@johnny.net.au>
2021-01-30 13:27:17 +1100

    📦🕺🏾 Hot reloading of CSS!
    Thanks, Dave Ceddia.
    
    https://daveceddia.com/tailwind-create-react-app/#production

--- src/components/VAppWrapper/index.tsx
+++ src/components/VAppWrapper/index.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react";
 
 const VAppWrapper: FunctionComponent = ({ children }) => (
-	<div className="m-4 bg-yellow-100">{children}</div>
+	<div className="p-16 m-4 text-5xl bg-yellow-100 font-jdcode">{children}</div>
 );
 
 export default VAppWrapper;
