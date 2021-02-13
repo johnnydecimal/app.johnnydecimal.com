6398e87d55721404940a758662e865799b6d6816
Johnny Noble <github-a3bd@johnny.net.au>
2021-02-13 16:19:25 +1100

    🗿 Tweaks; going to refactor SignInStateRouter…
    He’s a rock, as in this is a rock, a point in space you can come back to, “you’re my rock”, etc.

--- src/components/JDApp/Menu/index.tsx
+++ src/components/JDApp/Menu/index.tsx
@@ -1,4 +1,10 @@
+// === External ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===
 import { Link } from "@reach/router";
+
+// === Internal ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===
+import WaitOne from "components/WaitOne";
+
+// === Types    ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===
 import {
 	SignInContext,
 	SignInEvent,
@@ -14,34 +20,124 @@ interface MenuProps {
 	signInStateSend: (args: SignInEvent) => void;
 }
 
-const Menu = ({ signInState, signInStateSend }: MenuProps) => (
-	<div className="p-2">
-		<p>Sign in state: {signInState.value}</p>
-		<ul className="list-disc list-inside">
-			<li className="my-4">
-				<Link className="border-b-2 border-gray-800" to="/">
-					Main app
-				</Link>
-			</li>
-			<li className="my-4">
-				<Link className="border-b-2 border-gray-800" to="/account">
-					Your account
-				</Link>
-			</li>
-			<li className="my-4">
-				<button
-					className="text-red-600 border-b-2 border-red-600"
-					onClick={() => {
-						signInStateSend({
-							type: "TRY_SIGNOUT",
-						});
-					}}
-				>
-					Sign out
-				</button>
-			</li>
-		</ul>
-	</div>
-);
+// === Main ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===
+const Menu = ({ signInState, signInStateSend }: MenuProps) => {
+	switch (signInState.value) {
+		case "init" || "tryingSignIn" || "tryingSignUp" || "tryingSignOut":
+			return <WaitOne />;
+		case "notSignedIn":
+			return (
+				<div className="p-2">
+					<ul className="list-disc list-inside">
+						<li className="my-4">
+							<Link className="border-b-2 border-gray-800" to="/">
+								Sign in
+							</Link>
+						</li>
+						<li className="my-4">
+							<Link className="border-b-2 border-gray-800" to="/signup">
+								Sign up
+							</Link>
+						</li>
+						<li className="my-4">
+							<a
+								className="border-b-2 border-gray-800"
+								href="https://johnnydecimal.com"
+							>
+								Go to the main J.D site
+							</a>
+						</li>
+					</ul>
+				</div>
+			);
+		case "signedIn":
+			return (
+				<div className="p-2">
+					<p>Sign in state: {signInState.value}</p>
+					<ul className="list-disc list-inside">
+						<li className="my-4">
+							<Link className="border-b-2 border-gray-800" to="/">
+								Main app
+							</Link>
+						</li>
+						<li className="my-4">
+							<Link className="border-b-2 border-gray-800" to="/account">
+								Your account
+							</Link>
+						</li>
+						<li className="my-4">
+							<button
+								className="text-red-600 border-b-2 border-red-600"
+								onClick={() => {
+									signInStateSend({
+										type: "TRY_SIGNOUT",
+									});
+								}}
+							>
+								Sign out
+							</button>
+						</li>
+					</ul>
+				</div>
+			);
+		case "error":
+			// TODO: This isn't very good
+			return <div>WTF</div>;
+		default:
+			// impossible, all states are covered -- but TS needs a JSX element
+			return <div>WTF</div>;
+			break;
+	}
+
+	// if (signInState.value !== "signedIn") {
+	// 	return (
+	// 		<div className="p-2">
+	// 			<p>Sign in state: {signInState.value}</p>
+	// 			<ul className="list-disc list-inside">
+	// 				<li className="my-4">
+	// 					<Link className="border-b-2 border-gray-800" to="/">
+	// 						Home (?)
+	// 					</Link>
+	// 				</li>
+	// 				<li className="my-4">
+	// 					<Link className="border-b-2 border-gray-800" to="/">
+	// 						Er, home again?
+	// 					</Link>
+	// 				</li>
+	// 			</ul>
+	// 		</div>
+	// 	);
+	// } else {
+	// 	return (
+	// 		<div className="p-2">
+	// 			<p>Sign in state: {signInState.value}</p>
+	// 			<ul className="list-disc list-inside">
+	// 				<li className="my-4">
+	// 					<Link className="border-b-2 border-gray-800" to="/">
+	// 						Main app
+	// 					</Link>
+	// 				</li>
+	// 				<li className="my-4">
+	// 					<Link className="border-b-2 border-gray-800" to="/account">
+	// 						Your account
+	// 					</Link>
+	// 				</li>
+	// 				<li className="my-4">
+	// 					<button
+	// 						className="text-red-600 border-b-2 border-red-600"
+	// 						onClick={() => {
+	// 							signInStateSend({
+	// 								type: "TRY_SIGNOUT",
+	// 							});
+	// 						}}
+	// 					>
+	// 						Sign out
+	// 					</button>
+	// 				</li>
+	// 			</ul>
+	// 		</div>
+	// 	);
+	// }
+};
 
 export default Menu;
--- src/components/Layout/AppHeader/index.tsx
+++ src/components/Layout/AppHeader/index.tsx
@@ -1,5 +1,5 @@
 // === External ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===
-import { Link } from "@reach/router";
+import { Link, useLocation } from "@reach/router";
 
 // === Types    ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===
 import { SignInContext } from "components/SignInStateRouter/signInState.machine";
@@ -16,13 +16,8 @@ interface AppHeaderProps {
 
 // === Main ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===
 const AppHeader = ({ signInState, title }: AppHeaderProps) => {
-	switch (signInState.value) {
-		case "notSignedIn" || "try":
-			break;
+	const location = useLocation();
 
-		default:
-			break;
-	}
 	return (
 		<header
 			className="grid content-between grid-flow-col p-2 border-2 border-gray-800 font-jdcode"
@@ -32,12 +27,13 @@ const AppHeader = ({ signInState, title }: AppHeaderProps) => {
 			<div>{title}</div>
 			{/* <div className="hidden text-center sm:block">Johnny•Decimal</div>
 			<div className="block text-center sm:hidden">J•D</div> */}
-			<div className="text-right border-b-2 border-gray-800 justify-self-end">
-				<Link className="focus:outline-none" to="/menu">
-					Menu
-				</Link>
-			</div>
-			{/* <div>😎</div> */}
+			{location.pathname === "/menu" ? null : (
+				<div className="text-right border-b-2 border-gray-800 justify-self-end">
+					<Link className="focus:outline-none" to="/menu">
+						Menu
+					</Link>
+				</div>
+			)}
 		</header>
 	);
 };
